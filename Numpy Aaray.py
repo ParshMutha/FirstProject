c25b58c8ac8abea55f199b70494b6c227bdcf3a7
ParshMutha <muthaparsh@gmail.com>
2025-11-02 22:27:07 +0530

    First Commit

---
+++ Numpy Aaray.py
@@ -0,0 +1,20 @@
+import numpy as np
+import random 
+#Zeros array
+n1 = np.zeros((1,2))
+print(n1)
+
+#two dimensional aaray
+n2 = np.array([[1,2,3,4],[4,3,2,1]])
+print(n2)
+
+#One dimensional array
+n3 = np.array([1,2,3,4,5,6])
+
+#Random Numbers
+n4 = np.random.randint(1,100,5)
+print(n4)
+
+#Arange Method
+n5 = np.arange(10,20)
+print(n5)
